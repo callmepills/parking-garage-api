6c64c39f7e588c3cc53774359e307941e396e99e
Anthony Leveillee <aleveillee@gmail.com>
2018-02-04 17:44:43 -0500

    add postgres db for heroku

--- src/main/java/org/callmepills/parkinggarage/Spot.java
+++ src/main/java/org/callmepills/parkinggarage/Spot.java
@@ -11,7 +11,7 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 public class Spot {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String garage;
 	private String text;
